168a2e4acd7adde534181310c2bf1ecee98ebc23
ps2program <107313898+ps2program@users.noreply.github.com>
2025-09-22 23:09:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello, GitHub Actions!, Prahlad sahu"}
+    return {"message": "Hello, GitHub Actions!, Prahlad"}
 
 @app.get("/square/{number}")
 def square_number(number: int):
